f610ce497590bf1d6a2fe1b31b020a57f521839b
stradanya <100928077+stradanya@users.noreply.github.com>
2022-04-04 17:24:05 +0300

    Add files via upload

---
+++ java01.js
@@ -0,0 +1,11 @@
+'Приветик, Java'
+var myThing;
+myThings=5;
+myThing='это строка';
+var numberNine = 9;
+var strightNine = "9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "hello, ";
+var myName = "stradanya";
+greeting = myName;
